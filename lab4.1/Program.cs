c79f2cbe80042e17fd6d1c90a5362f03dcddb296
Sarah <Ms.sarahelizabethb@gmail.com>
2018-04-26 17:54:38 -0400

    first commit

--- lab4.1/Program.cs
+++ lab4.1/Program.cs
@@ -9,7 +9,37 @@ namespace lab4._1
     class Program
     {
         static void Main(string[] args)
+
         {
+            while (true)
+            {
+
+                Console.WriteLine("please enter a numer less than 100");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.WriteLine($"{"Number",-10}{"square",-10}{"cubed",-10}");
+                Console.WriteLine($"{"======",-10}{"======",-10}{"======",- 10}");
+                for (int i = 1; i <= number; i++)
+                {
+                int num = i;
+                int square = i * i;
+                int cube = i * i * i;
+
+                    Console.WriteLine($"{num,-10}{square,-10}{cube,-10}");
+
+                }
+                Console.WriteLine("do you want to continie: y/n");
+                string response = (Console.ReadLine());
+                if (response.ToLower() == "n")
+                {
+                    break;
+                }
+
+                Console.WriteLine("hope you had fun");
+
+            }
+
         }
     }
 }
